e3cd839194593ad8a2cb97a75c03b33c6187f944
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2023-07-27 15:49:38 -0300

    busca entre data

--- app/services/aluno_service_query.rb
+++ app/services/aluno_service_query.rb
@@ -13,11 +13,13 @@ class AlunoServiceQuery
 
         # alunos = alunos.where("idade between ? and ?", params[:idade_ini], params[:idade_fim]) if params[:idade_ini].present? && params[:idade_fim].present?
 
+        alunos = alunos.where("created_at >= ?", params[:data_ini]) if params[:data_ini].present?
+        alunos = alunos.where("created_at <= ?", params[:data_fim]) if params[:data_fim].present?
+
         if params[:ids].present?
             array_ids = params[:ids].split(",").map{|s| s.to_i}.find_all{|n| n > 0} rescue []
             alunos = alunos.where(id: array_ids)
         end
-
 
         puts "===================="
         puts alunos.to_sql
